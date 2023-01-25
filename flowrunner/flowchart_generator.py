6dc0a5e519e1648b2ba6f696f942f9de61f2da3e
prithvijitguha <prithvijit_guha2@hotmail.com>
2023-01-25 22:36:33 +0530

    added Jinja templater

---
+++ flowrunner/flowchart_generator.py
@@ -0,0 +1,28 @@
+"""Module to generate html pages"""
+
+from jinja2 import Environment, FileSystemLoader
+import flow_example
+from runner import FlowRunner
+from system.logger import logger
+import os
+
+flow = FlowRunner(flow_example)
+flow.validate()
+flow._traverse_graph()
+
+
+root = os.path.dirname(os.path.abspath(__file__))
+templates_dir = os.path.join(root, 'templates')
+environment = Environment(loader=FileSystemLoader(templates_dir))
+template = environment.get_template("base.html")
+
+
+filename = f"flowchart_{flow_example.__name__}.html"
+logger.debug("Saving file: %s", filename)
+content = template.render(
+    nodes=flow.nodes,
+    edges=flow.edges
+)
+with open(filename, mode="w", encoding="utf-8") as message:
+    message.write(content)
+    logger.debug("Saved file %s", filename)
