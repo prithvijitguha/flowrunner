f6f64dd72b6fd3676cac05b327d00f6cab3dfce5
prithvijitguha <prithvijit_guha2@hotmail.com>
2023-02-26 22:31:04 +0530

    modified cli script and added to pyproject toml

--- flowrunner/cli.py
+++ flowrunner/cli.py
@@ -2,13 +2,10 @@
 import click
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
+@click.option('--validate', help='Validate Flow')
+def validate_flow(flow):
     """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+    click.secho("Hello World")
 
 if __name__ == '__main__':
-    hello()
+    validate_flow()
--- runner.py
+++ runner.py
@@ -1,8 +1,8 @@
 from class_flow_example import FlowExample
 
+FlowExample().validate_flow() # we validate the flow
 
-FlowExample().validate_flow()
-FlowExample().validate_flow_with_error()
+FlowExample().validate_flow_with_error() # we validate the flow and throw an exception if its not valid
 
 
 
