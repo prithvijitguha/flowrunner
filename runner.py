5c8be8fb4b523d88d5cd9c070db3bd4acb883b65
prithvijitguha <prithvijit_guha2@hotmail.com>
2023-02-26 22:17:31 +0530

    cleaned up runner.py

--- runner.py
+++ runner.py
@@ -1,23 +1,8 @@
-import flow_example
-
 from class_flow_example import FlowExample
-from flowrunner.core.graph import GraphOptions, Graph
 
-
-
-# graph_options = GraphOptions(FlowExample)
-# #print(graph_options)
-
-# graph = Graph(graph_options=graph_options)
-
-# graph._arrange_graph()
-
-# graph.run_flow()
-#graph._traverse_graph()
 
 FlowExample().validate_flow()
 FlowExample().validate_flow_with_error()
-#FlowExample().run_flow()
 
 
 
