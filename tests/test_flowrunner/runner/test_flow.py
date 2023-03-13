948f229591a5389e41d3ab081d65a127ec73f207
prithvijitguha <prithvijit_guha2@hotmail.com>
2023-03-13 21:53:13 +0530

    checking refactor for data store output, we cannot assume that all flows will write to external mounted resources

--- tests/test_flowrunner/runner/test_flow.py
+++ tests/test_flowrunner/runner/test_flow.py
@@ -1,66 +1,141 @@
 # -*- coding: utf-8 -*-
-from flowrunner.core.decorators import end, start, step
-from flowrunner.runner.flow import BaseFlow, FlowRunner
+import pandas as pd
+import pytest
+
+from flowrunner import BaseFlow, FlowRunner, end, start, step
 
 
-class FlowExample(BaseFlow):
+class ExampleFlow(BaseFlow):
     @start
-    @step(next=["middle_func", "another_middle_func"])
-    def first_func(self):
-        """This function is the start of our workflow
-        where we extract 3 dataframes"""
-        x = 2
-        self.data_store["first_func"] = x
-        print("first_func output", x)
+    @step(next=["method2", "method3"])
+    def method1(self):
+        self.a = 1
+        return self.a
 
-    @step(next="model_func")
-    def middle_func(self):
-        """This function is the middle where we
-        filter + transform stuff"""
-        value_from_first = self.data_store["first_func"]
-        self.data_store["middle_func"] = value_from_first * 3
-        print("middle_func output", self.data_store["middle_func"])
+    @step(next=["method4"])
+    def method2(self):
+        self.a += 1
+        return self.a
 
-    @step(next="model_func")
-    def another_middle_func(self):
-        """This function is the middle where we
-        filter + transform stuff"""
-
-        value_from_first = self.data_store["first_func"]
-        self.data_store["another_middle_func"] = value_from_first
-        print("another_middle_func output", value_from_first)
-
-    @step(next="end_func")
-    def model_func(self):
-        """This function does model training"""
-        y = self.data_store["middle_func"]
-        x = self.data_store["another_middle_func"]
-        self.data_store["model_func"] = x + y
-        print("model_func output", x)
-        print("model_func output", y)
+    @step(next=["method4"])
+    def method3(self):
+        self.a += 2
+        return self.a
 
     @end
     @step
-    def end_func(self):
-        """This function is the end where
-        we write data into a table"""
-        final_value = self.data_store["model_func"]
-        print("end_func output", final_value)
+    def method4(self):
+        self.a += 3
+        print("output of flow is:", self.a)
+        return self.a
 
 
 def test_validate_flow():
     """Run validate flow"""
-    FlowExample().validate()
+    ExampleFlow().validate()
 
 
 def test_validate_flow_with_error():
-    FlowExample().validate_with_error()  # we validate the flow and throw an exception if its not valid
+    ExampleFlow().validate_with_error()  # we validate the flow and throw an exception if its not valid
 
 
 def test_flowrunner():
-    flow_runner = FlowRunner(FlowExample)
+    flow_runner = FlowRunner(ExampleFlow)
     flow_runner.run()
 
 
 def test_base_flow_run_flow():
-    FlowExample().run()
+    ExampleFlow().run()
+
+
+@pytest.mark.skip("issues with pandas testing")
+def test_data_store():
+    """Test to check if data store actually stores output"""
+    flow_instance = ExampleFlow()
+    print(id(flow_instance))
+    flow_instance.run()
+    print(flow_instance.data_store)
+
+
+class ExamplePandas(BaseFlow):
+    @start
+    @step(next=["transformation_function_1", "transformation_function_2"])
+    def create_data(self):
+        """
+        This method we create the dataset we are going use. In real use cases,
+        you'll have to read from a source (csv, parquet, etc)
+
+        For this example we create two dataframes for students ranked by marked scored
+        for when they attempted the example on 1st January 2023 and 12th March 2023
+
+        After creating the dataset we pass it to the next methods
+
+        - transformation_function_1
+        - transformation_function_2
+        """
+        data1 = {"Name": ["Hermione", "Harry", "Ron"], "marks": [100, 85, 75]}
+
+        data2 = {"Name": ["Hermione", "Ron", "Harry"], "marks": [100, 90, 80]}
+
+        df1 = pd.DataFrame(data1, index=["rank1", "rank2", "rank3"])
+
+        df2 = pd.DataFrame(data2, index=["rank1", "rank2", "rank3"])
+
+        self.input_data_1 = df1
+        self.input_data_2 = df2
+
+    @step(next=["append_data"])
+    def transformation_function_1(self):
+        """
+        Here we add a snapshot_date to the input dataframe of 2023-03-12
+        """
+        transformed_df = self.input_data_1
+        transformed_df.insert(1, "snapshot_date", "2023-03-12")
+        self.transformed_df_1 = transformed_df
+
+    @step(next=["append_data"])
+    def transformation_function_2(self):
+        """
+        Here we add a snapshot_date to the input dataframe of 2023-01-01
+        """
+        transformed_df = self.input_data_2
+        transformed_df.insert(1, "snapshot_date", "2023-01-01")
+        self.transformed_df_2 = transformed_df
+
+    @step(next=["show_data"])
+    def append_data(self):
+        """
+        Here we append the two dataframe together
+        """
+        self.final_df = pd.concat([self.transformed_df_1, self.transformed_df_2])
+
+    @end
+    @step
+    def show_data(self):
+        """
+        Here we show the new final dataframe of aggregated data. However in real use cases. It would
+        be more likely to write the data to some final layer/format
+        """
+        print(self.final_df)
+
+
+@pytest.fixture(scope="module")
+def pandas_expected_df():
+    data1 = {"Name": ["Hermione", "Harry", "Ron"], "marks": [100, 85, 75]}
+
+    data2 = {"Name": ["Hermione", "Ron", "Harry"], "marks": [100, 90, 80]}
+
+    df1 = pd.DataFrame(data1, index=["rank1", "rank2", "rank3"])
+
+    df2 = pd.DataFrame(data2, index=["rank1", "rank2", "rank3"])
+
+    df1.insert(1, "snapshot_date", "2023-03-12")
+    df2.insert(1, "snapshot_date", "2023-01-01")
+    return pd.concat([df1, df2])
+
+
+@pytest.mark.skip("issues with pandas testing")
+def test_pandas_example(pandas_expected_df):
+    pandas_example = ExamplePandas.run()
+    pandas_run = pandas_example.run()
+    pandas_run.final_df == pandas_expected_df
