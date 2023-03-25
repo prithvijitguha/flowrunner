5f6c3e17e27a2f6db93ed072ca44053e40fb8e79
prithvijitguha <prithvijit_guha2@hotmail.com>
2023-03-25 23:03:59 +0530

    removed docs notebook

--- examples/example.py
+++ examples/example.py
@@ -4,17 +4,15 @@ from flowrunner import BaseFlow, end, start, step
 
 class ExampleFlow(BaseFlow):
     @start
-    @step(next=["method2", "method3"])
+    @step(next=['method2', 'method3'])
     def method1(self):
-        """Example of a method with a docstring which
-        will become description"""
         self.a = 1
 
-    @step(next=["method4"])
+    @step(next=['method4'])
     def method2(self):
         self.a += 1
 
-    @step(next=["method4"])
+    @step(next=['method4'])
     def method3(self):
         self.a += 2
 
@@ -22,7 +20,8 @@ class ExampleFlow(BaseFlow):
     @step
     def method4(self):
         self.a += 3
-        print(self.a)
+        print("output of flow is:", self.a)
+
 
 
 class ExampleFlow2(BaseFlow):
