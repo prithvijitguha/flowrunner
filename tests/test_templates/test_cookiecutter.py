260122d6824881a97738549239fd291bd5d4524b
prithvijitguha <prithvijit_guha2@hotmail.com>
2023-04-09 17:27:33 +0530

    updated cookie cutter

--- tests/test_templates/test_cookiecutter.py
+++ tests/test_templates/test_cookiecutter.py
@@ -1,10 +1,24 @@
-# -*- coding: utf-8 -*-
 """Module to test that cookie cutter is working as expected"""
+import pytest
 
-def test_bake_project(cookies):
-    result = cookies.bake()
+@pytest.fixture
+def custom_template(tmpdir):
+    """Function to create cookie cutter template"""
+    template = tmpdir.ensure("cookiecutter-template", dir=True)
+    template.join("cookiecutter.json").write('{"repo_name": "example-project"}')
+
+    repo_dir = template.ensure("{{cookiecutter.repo_name}}", dir=True)
+    repo_dir.join("README.rst").write("{{cookiecutter.repo_name}}")
+
+    return template
+
+
+def test_bake_custom_project(cookies, custom_template):
+    """Test for 'cookiecutter-template'."""
+    result = cookies.bake(template=str(custom_template))
 
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project.basename == "Flowrunner_Project_Simple"
-    assert result.project.isdir()
+
+    assert result.project_path.name == "example-project"
+    assert result.project_path.is_dir()
